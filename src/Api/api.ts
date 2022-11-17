7f49b878189b8723864d9bef75466f1b954410c9
Tarny94 <102902000+Tarny94@users.noreply.github.com>
2022-11-17 11:06:44 +0200

    modified api

--- src/Api/api.ts
+++ src/Api/api.ts
@@ -1,8 +1,5 @@
 const port = 6060;
 
-console.log(window.location.protocol);
-console.log(window.location.hostname);
-
 export const getApiUrl = (route: string) => {
-  return `${window.location.protocol}//${window.location.hostname}:${port}/${route}`;
+  return `localhost:${port}/${route}`;
 };
