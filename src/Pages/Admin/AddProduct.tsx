7f734c56eee9b562715c02a3cf9a9ae18492ef70
Tarny94 <102902000+Tarny94@users.noreply.github.com>
2022-11-23 13:41:16 +0200

    added states for inputs values

--- src/Pages/Admin/AddProduct.tsx
+++ src/Pages/Admin/AddProduct.tsx
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "../../Design/Input";
 import Button from "../../Design/Button";
 import "../Admin/Products.scss";
 
 const AddProduct = () => {
+  const [image, setImage] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState(0);
+  const [discount, setDiscount] = useState(0);
+
+  const product = {
+    image,
+    title,
+    description,
+    price,
+    discount,
+  };
+
   return (
     <div className="add-product-container">
       <div className="add-product-details">
         <h1 className="add-product-title">ADD PRODUCT</h1>
         <div className="add-product-inputs">
-          <Input setValue={undefined} placeholder="Images" />
-          <Input setValue={undefined} placeholder="Title" />
-          <Input setValue={undefined} placeholder="Description" />
-          <Input setValue={undefined} placeholder="Price" />
-          <Input setValue={undefined} placeholder="Discount" />
+          <Input setValue={setImage} placeholder="Images" />
+          <Input setValue={setTitle} placeholder="Title" />
+          <Input setValue={setDescription} placeholder="Description" />
+          <Input setValue={setPrice} placeholder="Price" type="number" />
+          <Input setValue={setDiscount} placeholder="Discount" type="number" />
         </div>
         <div className="add-product-submit">
           <Button
