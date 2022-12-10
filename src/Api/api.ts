e2b64ceb638bebb5f5a7293645673069973e7671
Tarny94 <102902000+Tarny94@users.noreply.github.com>
2022-12-10 15:11:52 +0200

    refactor api

--- src/Api/api.ts
+++ src/Api/api.ts
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:6060";
+const API_URL = process.env.API_URL || "http://localhost:6060";
 
 export const getApiUrl = (route: string) => {
   return `${API_URL}/${route}`;
